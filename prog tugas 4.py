eadb12280aa8b10bf1710ac9e5b2bb2fc0477e0f
Suryaaa-Ramdhani <surya.ramdhani144@sma.belajar.id>
2024-11-11 21:05:28 +0800

    Add files via upload

---
+++ prog tugas 4.py
@@ -0,0 +1,14 @@
+#hitunglah luas persegi
+s = 44
+s = 24
+#rumus mencari luas persegi
+luas = s*s
+
+print("nilai a adalah : ", s)
+print("nilai t adalah : ", s)
+print("nilai luas adalah : ", luas )
+
+#hasil dari hitung luas 
+s = 44
+s = 24
+luas = 576
